38b1cb32710caffc74dd7f3ce22ac9613c458544
Subham5778 <167920208+Subham5778@users.noreply.github.com>
2024-08-24 18:40:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+import "./style.css";
+
+import products from "./api/products.json";
+import { showProductContainer } from "./homeProductCards";
+
+// Define a function name show productContainer that takes an array of product as input
+showProductContainer(products);
